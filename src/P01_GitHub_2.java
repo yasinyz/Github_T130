b0f8b589d1c4d5955be0fd313c2a366feaf86c6f
yasinyz <153136263+yasinyz@users.noreply.github.com>
2023-12-11 15:25:28 +0300

    SecondFiles

---
+++ src/P01_GitHub_2.java
@@ -0,0 +1,8 @@
+public class P01_GitHub_2 {
+
+    public static void main(String[] args) {
+        int a=128;
+        int b=256;
+        System.out.println("İki sayının toplamı: "+(a+b));
+    }
+}
